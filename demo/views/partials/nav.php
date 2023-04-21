5b16e12046ddf0c365348d35c29dc55fcaa1184a
Precious Smith <98830144+Capn-Precious@users.noreply.github.com>
2023-04-21 13:45:59 -0500

    removing .php and add url access to utils

--- demo/views/partials/nav.php
+++ demo/views/partials/nav.php
@@ -9,9 +9,9 @@
                     <div class="ml-10 flex items-baseline space-x-4">
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                         <a href="/" class="<?= utlIs('/') ? 'bg-gray-900' : 'text-gray-300'; ?>  hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Home</a>
-                        <a href="/about" class="<?= utlIs('/about.php') ? 'bg-gray-900' : 'text-gray-300'; ?> hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
-                        <a href="/mission" class="<?= utlIs('/contact.php') ? 'bg-gray-900' : 'text-gray-300'; ?> hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Our Mission</a>
-                        <a href="/contact" class="<?= utlIs('/mission.php') ? 'bg-gray-900' : 'text-gray-300'; ?> hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a>
+                        <a href="/about" class="<?= utlIs('/about') ? 'bg-gray-900' : 'text-gray-300'; ?> hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
+                        <a href="/mission" class="<?= utlIs('/contact') ? 'bg-gray-900' : 'text-gray-300'; ?> hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Our Mission</a>
+                        <a href="/contact" class="<?= utlIs('/mission') ? 'bg-gray-900' : 'text-gray-300'; ?> hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a>
 
                     </div>
                 </div>
